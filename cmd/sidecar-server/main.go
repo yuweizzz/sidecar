c8f18f1633dce3584dace7dc53f90802f8e887d3
yuweizzz <yuwei764969238@gmail.com>
2022-04-17 23:10:07 +0800

    perf: handle lock file in sidecar-server

--- cmd/sidecar-server/main.go
+++ cmd/sidecar-server/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,6 +15,20 @@ import (
 )
 
 func main() {
+	pid := sidecar.ReadLock()
+	// if lock exist
+	if pid != 0 {
+		alive := sidecar.DetectProcess(pid)
+		// if process alive
+		if alive {
+			fmt.Println("Maybe Server is running and pid is", pid)
+			fmt.Println("If Server is not running, remove sidecar-server.lock and retry.")
+			panic("Run failed, sidecar-server.lock exist.")
+			// if process not alive
+		} else {
+			sidecar.RemoveLock()
+		}
+	}
 	var cfg *sidecar.Config
 	config_file_path := sidecar.DetectFile("conf.toml")
 	if config_file_path == "" {
@@ -41,18 +56,18 @@ func main() {
 	if pri_file_path := sidecar.DetectFile("certificate/sidecar.pri"); pri_file_path == "" {
 		pri_fd := sidecar.CreateFileIfNotExist("certificate/sidecar.pri")
 		pri = sidecar.GenAndSavePriKey(pri_fd)
-		sidecar.LogRecord(log_fd, "info", "Generate new privatekey.")
+		sidecar.LogRecord(log_fd, "info", "Generate new privatekey......")
 	} else {
 		pri = sidecar.ReadPriKey("certificate/sidecar.pri")
-		sidecar.LogRecord(log_fd, "info", "Use exist privatekey.")
+		sidecar.LogRecord(log_fd, "info", "Use exist privatekey......")
 	}
 	if crt_file_path := sidecar.DetectFile("certificate/sidecar.crt"); crt_file_path == "" {
 		crt_fd := sidecar.CreateFileIfNotExist("certificate/sidecar.crt")
 		crt = sidecar.GenAndSaveRootCert(crt_fd, pri)
-		sidecar.LogRecord(log_fd, "info", "Generate new certificate.")
+		sidecar.LogRecord(log_fd, "info", "Generate new certificate......")
 	} else {
 		crt = sidecar.ReadRootCert("certificate/sidecar.crt")
-		sidecar.LogRecord(log_fd, "info", "Use exist certificate.")
+		sidecar.LogRecord(log_fd, "info", "Use exist certificate......")
 	}
 	server := &http.Server{
 		Addr: ":443",
@@ -75,15 +90,19 @@ func main() {
 	}
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
+	pid = os.Getpid()
+	sidecar.WriteLock(pid)
+	fmt.Println("Now Server is running and pid is", pid)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go proxy.ListenAndServe()
 	go func() {
 		<-sigs
 		done <- true
 	}()
-	sidecar.LogRecord(log_fd, "info", "awaiting signal......")
+	sidecar.LogRecord(log_fd, "info", "Awaiting signal......")
 	go server.ListenAndServeTLS("", "")
 	<-done
-	sidecar.LogRecord(log_fd, "info", "except signal, exiting......")
+	sidecar.LogRecord(log_fd, "info", "Except signal, exiting......")
+	sidecar.RemoveLock()
 	defer log_fd.Close()
 }
