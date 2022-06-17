3e4de8ff2aa2d4b8e0acb88022095cc015ef7a36
yuweizzz <yuwei764969238@gmail.com>
2022-06-17 21:49:25 +0800

    fix: http request lost header cause CORS error

--- handle.go
+++ handle.go
@@ -7,11 +7,17 @@ import (
 )
 
 func HandleHttp(server string, subpath string, key string, value string, writer http.ResponseWriter, in_req *http.Request) {
-	var body io.Reader
-	dest_url := server + "/" + subpath + "/" + in_req.Host + in_req.URL.String()
-	out_req, _ := http.NewRequest(in_req.Method, dest_url, body)
-	out_req.Header.Add(key, value)
-	resp, _ := http.DefaultTransport.RoundTrip(out_req)
+	dest_url := in_req.URL
+	dest_url.Scheme = "https"
+	dest_url.Host = server
+	in_path := dest_url.Path
+	dest_url.Path = "/" + subpath + "/" + in_req.Host + in_path
+	in_req.Host = server
+	in_req.Header.Add(key, value)
+	resp, _ := http.DefaultTransport.RoundTrip(in_req)
+	for key, value := range resp.Header {
+		writer.Header().Set(key, value[0])
+	}
 	writer.WriteHeader(resp.StatusCode)
 	io.Copy(writer, resp.Body)
 }
