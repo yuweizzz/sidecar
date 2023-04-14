79d8acf49a8806289f1486c4181d207bc41b5b00
yuweizzz <yuwei764969238@gmail.com>
2023-04-14 21:47:32 +0800

    feat: support remote server operate in cmdline

--- cmd/sidecar-server/main.go
+++ cmd/sidecar-server/main.go
@@ -8,46 +8,52 @@ import (
 	"path"
 	"runtime"
 	"strconv"
+	"syscall"
 
 	"github.com/yuweizzz/sidecar"
 )
 
 func help(filename string) {
 	fmt.Println("Usage:")
-	fmt.Println("      ", filename, "start [-conf tomlfile] [-dir workdir] [-daemon]")
-	fmt.Println("      ", filename, "create-nginx-conf [-conf tomlfile] [-dir outputdir]")
-	fmt.Println("      ", filename, "stop [-dir workdir]")
+	fmt.Println("      ", filename, "client -action {start|stop} [-conf ./config.toml]")
+	fmt.Println("      ", filename, "server -action {start|stop|create-nginx-conf} [-conf ./config.toml]")
 }
 
 func readConfig(path string) *sidecar.Config {
-	config_file_path := sidecar.DetectFile(path)
-	if config_file_path == "" {
-		panic("Run failed, conf file not exist.")
+	configPath := sidecar.DetectFile(path)
+	if configPath == "" {
+		panic("Run failed, config.toml not exist.")
 	} else {
-		return sidecar.ReadConfig(config_file_path)
+		return sidecar.ReadConfig(configPath)
 	}
 }
 
 func main() {
+	// basic info
+	_, filename := path.Split(os.Args[0])
 	pwd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
-	tomlPathForStart := startCmd.String("conf", pwd+"/conf.toml", "conf")
-	workDirForStart := startCmd.String("dir", pwd, "dir")
-	runDaemon := startCmd.Bool("daemon", false, "daemon")
-	stopCmd := flag.NewFlagSet("stop", flag.ExitOnError)
-	workDirForStop := stopCmd.String("dir", pwd, "dir")
-	createNginxConfCmd := flag.NewFlagSet("create-nginx-conf", flag.ExitOnError)
-	tomlPathForCreate := createNginxConfCmd.String("conf", pwd+"/conf.toml", "conf")
-	workDirForCreate := createNginxConfCmd.String("dir", pwd, "dir")
-	_, filename := path.Split(os.Args[0])
+	// client
+	clientCmd := flag.NewFlagSet("client", flag.ExitOnError)
+	configClientPath := clientCmd.String("conf", pwd+"/config.toml", "the path of sidecar conf")
+	clientAction := clientCmd.String("action", "", "action must in ['start', 'stop']")
+	// server
+	serverCmd := flag.NewFlagSet("server", flag.ExitOnError)
+	configServerPath := serverCmd.String("conf", pwd+"/config.toml", "the path of sidecar conf")
+	serverAction := serverCmd.String("action", "", "action must in ['start', 'stop', 'create-nginx-conf']")
 
+	// for run as daemon
 	if len(os.Args) < 2 {
 		if os.Getenv("SPECIAL_MARK") == "ENABLED" {
 			cfg := readConfig(os.Getenv("CONF_PATH"))
-			run(cfg, os.Getenv("WORKDIR"), true)
+			switch os.Getenv("TYPE") {
+			case "client":
+				runClient(cfg)
+			case "server":
+				runServer(cfg)
+			}
 		} else {
 			help(filename)
 		}
@@ -55,64 +61,123 @@ func main() {
 	}
 
 	switch os.Args[1] {
-	case "start":
-		startCmd.Parse(os.Args[2:])
-		cfg := readConfig(*tomlPathForStart)
-		if *runDaemon {
-			cmd := &exec.Cmd{
-				Path:   os.Args[0],
-				Env:    []string{"SPECIAL_MARK=ENABLED", "CONF_PATH=" + *tomlPathForStart, "WORKDIR=" + *workDirForStart},
-				Stdout: os.Stdout,
-				Stderr: os.Stdout,
+	case "client":
+		clientCmd.Parse(os.Args[2:])
+		cfg := readConfig(*configClientPath)
+		switch *clientAction {
+		case "start":
+			if cfg.Client.RunAsDaemon {
+				cmd := &exec.Cmd{
+					Path:   os.Args[0],
+					Env:    []string{"SPECIAL_MARK=ENABLED", "CONF_PATH=" + *configClientPath, "TYPE=client"},
+					Stdout: os.Stdout,
+					Stderr: os.Stdout,
+				}
+				err := cmd.Start()
+				if err != nil {
+					panic(err)
+				}
+				os.Exit(0)
+			} else {
+				runClient(cfg)
 			}
-			err := cmd.Start()
-			if err != nil {
-				panic(err)
+		case "stop":
+			lockPath := cfg.Client.WorkDir + "/sidecar.lock"
+			pid := sidecar.ReadLock(lockPath)
+			// if lock exist
+			if pid != 0 {
+				proc, _ := os.FindProcess(pid)
+				if runtime.GOOS == "windows" {
+					sidecar.RemoveLock(lockPath)
+					proc.Kill()
+				} else {
+					proc.Signal(syscall.SIGINT)
+				}
+				fmt.Println("Now Server is stopped.")
+			} else {
+				fmt.Println("Now sidecar.lock is not exist, server is stopped")
 			}
-			os.Exit(0)
-		} else {
-			run(cfg, *workDirForStart, false)
+		default:
+			help(filename)
 		}
-	case "stop":
-		stopCmd.Parse(os.Args[2:])
-		lockPath := *workDirForStop + "/sidecar-server.lock"
-		pid := sidecar.ReadLock(lockPath)
-		// if lock exist
-		if pid != 0 {
-			proc, _ := os.FindProcess(pid)
-			if runtime.GOOS == "windows" {
-				sidecar.RemoveLock(lockPath)
+	case "server":
+		serverCmd.Parse(os.Args[2:])
+		cfg := readConfig(*configServerPath)
+		switch *serverAction {
+		case "start":
+			if cfg.Server.RunAsDaemon {
+				cmd := &exec.Cmd{
+					Path:   os.Args[0],
+					Env:    []string{"SPECIAL_MARK=ENABLED", "CONF_PATH=" + *configServerPath, "TYPE=server"},
+					Stdout: os.Stdout,
+					Stderr: os.Stdout,
+				}
+				err := cmd.Start()
+				if err != nil {
+					panic(err)
+				}
+				os.Exit(0)
+			} else {
+				runServer(cfg)
 			}
-			proc.Kill()
-			fmt.Println("Now Server is stopped.")
-		} else {
-			fmt.Println("Now sidecar-server.lock is not exist, server is stopped")
+		case "stop":
+			lockPath := cfg.Server.WorkDir + "/sidecar.lock"
+			pid := sidecar.ReadLock(lockPath)
+			if pid != 0 {
+				proc, _ := os.FindProcess(pid)
+				if runtime.GOOS == "windows" {
+					sidecar.RemoveLock(lockPath)
+					proc.Kill()
+				} else {
+					proc.Signal(syscall.SIGINT)
+				}
+				fmt.Println("Now Server is stopped.")
+			} else {
+				fmt.Println("Now sidecar.lock is not exist, server is stopped")
+			}
+		case "create-nginx-conf":
+			sidecar.RenderTemplateByConfig(cfg.Server.WorkDir, cfg)
+		default:
+			help(filename)
 		}
-	case "create-nginx-conf":
-		createNginxConfCmd.Parse(os.Args[2:])
-		cfg := readConfig(*tomlPathForCreate)
-		sidecar.RenderTemplateByConfig(*workDirForCreate, cfg)
 	default:
 		help(filename)
 	}
 }
 
-func run(cfg *sidecar.Config, workdir string, backgroud bool) {
+func runClient(cfg *sidecar.Config) {
 	daemon := &sidecar.Daemon{
-		WorkDir:      workdir,
-		CertPath:     workdir + "/sidecar.crt",
-		PriKeyPath:   workdir + "/sidecar.pri",
-		LockFilePath: workdir + "/sidecar-server.lock",
-		LogLevel:     cfg.Sidecar.LogLevel,
+		WorkDir:      cfg.Client.WorkDir,
+		CertPath:     cfg.Client.CertPath,
+		PriKeyPath:   cfg.Client.PriKeyPath,
+		LockFilePath: cfg.Client.WorkDir + "/sidecar.lock",
+		LogLevel:     cfg.Client.LogLevel,
 	}
-	daemon.Perpare(backgroud)
-	pac := sidecar.NewPac(cfg)
-	proxy := sidecar.NewProxyServer(cfg.Sidecar.OnlyListenIPv4, cfg.Sidecar.ProxyPort, daemon.Logger, pac)
+	daemon.Perpare(cfg.Client.RunAsDaemon)
+	daemon.LoadCertAndPriKey()
+	pac := sidecar.NewPac(cfg.Client.RemoteServers[0], cfg.Client.GfwListUrl, cfg.Client.CustomProxyHosts)
+	proxy := sidecar.NewProxyServer(cfg.Client.OnlyListenIPv4, cfg.Client.ProxyPort, daemon.Logger, pac)
 	cache := sidecar.NewCertLRU(daemon.Cert, daemon.PriKey)
-	forwarder := sidecar.NewNextProxyServer(proxy.Listener, cache, daemon.Logger, cfg.RemoteProxy.Server, cfg.RemoteProxy.ComplexPath, cfg.RemoteProxy.CustomHeaders)
+	forwarder := sidecar.NewNextProxyServer(proxy.Listener, cache, daemon.Logger,
+		cfg.Client.RemoteServers[0].Host, cfg.Client.RemoteServers[0].ComplexPath, cfg.Client.RemoteServers[0].CustomHeaders)
 	sidecar.Info("Now Server is running and pid is " + strconv.Itoa(daemon.Pid))
 	go proxy.Run()
 	go forwarder.Run()
-	forwarder.WatchSignal()
-	daemon.Clean()
+	daemon.WatchSignal()
+}
+
+func runServer(cfg *sidecar.Config) {
+	daemon := &sidecar.Daemon{
+		WorkDir:      cfg.Server.WorkDir,
+		CertPath:     cfg.Server.CertPath,
+		PriKeyPath:   cfg.Server.PriKeyPath,
+		LockFilePath: cfg.Server.WorkDir + "/sidecar.lock",
+		LogLevel:     cfg.Server.LogLevel,
+	}
+	daemon.Perpare(cfg.Server.RunAsDaemon)
+	server := sidecar.NewRemoteServer(cfg.Server.ServerPort, daemon.Logger, daemon.CertPath, daemon.PriKeyPath,
+		cfg.Server.OnlyListenIPv4, cfg.Server.ComplexPath, cfg.Server.CustomHeaders)
+	sidecar.Info("Now Server is running and pid is " + strconv.Itoa(daemon.Pid))
+	go server.Run()
+	daemon.WatchSignal()
 }
